6f9d9ddfc3093b766d9a79bdec966ba7b4207afd
Yevhen-Code <47399557+Yevhen-Code@users.noreply.github.com>
2019-02-06 15:26:15 -0600

    Create add_test_contact_new.py

---
+++ test/add_test_contact_new.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from model.contact import Contact
+
+
+def test_add_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.create_contact(
+            Contact(name="Yevhen", lastname="Hurtovyi", company="Sidley", address="Chicago", mobile="7733311608",
+                    email="y.hurtovyi@yahoo.com"))
+    app.session.logout()
+
+
+def test_add_digits_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.create_contact(Contact(name="1", lastname="2", company="3", address="4",
+                                    mobile="5", email="6"))
+    app.session.logout()
